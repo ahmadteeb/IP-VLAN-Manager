645438544e4c74d85851850d181d921e144fa144
Ahmad El-Khatib <ahmadelkhatib1998@gmail.com>
2026-01-13 22:28:14 +0300

    fix permissions and enhance the UI

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     """Base configuration"""
     HOST = os.environ.get('HOST') or 'localhost'
     PORT = os.environ.get('PORT') or 5000
-    DEBUG = os.environ.get('DEBUG') or TRUE
+    DEBUG = os.environ.get('DEBUG') or False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ip_vlan_manager.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
